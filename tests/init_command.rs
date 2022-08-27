e2455c9c2c3eb1d4802720fb987f1333d1fd1695
Matthias Beyer <mail@beyermatthias.de>
2022-08-27 19:15:57 +0200

    Add test whether fragments dir exists after init
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/init_command.rs
+++ tests/init_command.rs
@@ -27,3 +27,30 @@ fn init_command_creates_config_file() {
     }
 }
 
+#[test]
+fn init_command_creates_fragment_dir() {
+    let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
+
+    if !std::process::Command::new("git")
+        .args(&["init"])
+        .current_dir(&temp_dir)
+        .status()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to git-init");
+    }
+
+    Command::cargo_bin("cargo-changelog")
+        .unwrap()
+        .args(&["init"])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+
+    let config_file_path = temp_dir.path().join(".changelogs");
+    if !config_file_path.exists() {
+        panic!("Fragments directory '.changelogs' does not exist after `cargo-changelog init`");
+    }
+}
+
