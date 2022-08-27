72d7b7390ab344a6d28c42eb743b87915f56386c
Matthias Beyer <mail@beyermatthias.de>
2022-08-27 17:50:41 +0200

    Restructure CLI
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ src/cli.rs
@@ -0,0 +1,43 @@
+use clap::Parser;
+use clap::Subcommand;
+
+/// Get CLI args via `clap` while also handling when we are invoked as a cargo
+/// subcommand
+pub fn get_args() -> Args {
+    // If we are invoked by cargo as `cargo changelog`, the second arg will
+    // be "changelog". Remove it before passing args on to clap. If we are
+    // not invoked as a cargo subcommand, it will not be part of args at all, so
+    // it is safe to filter it out also in that case.
+    let args = std::env::args_os().filter(|x| x != "changelog");
+    Args::parse_from(args)
+}
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+pub struct Args {
+    #[clap(subcommand)]
+    pub(crate) command: Command,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    New {
+    },
+
+    #[clap(subcommand)]
+    Generate(VersionSpec),
+
+    Release,
+}
+
+#[derive(Subcommand)]
+pub enum VersionSpec {
+    Patch,
+    Minor,
+    Major,
+    Custom {
+        #[clap(value_parser)]
+        custom: String,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,33 @@
-use clap::{Subcommand, Parser};
-
+mod cli;
 mod config;
 mod fragment;
 
-#[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
-#[clap(propagate_version = true)]
-struct Args {
-    #[clap(subcommand)]
-    command: Command,
-}
-
-#[derive(Subcommand)]
-enum Command {
-    New {
-    },
-
-    #[clap(subcommand)]
-    Release(ReleaseSubcommand),
-
-    Generate,
-
-    #[clap(subcommand)]
-    GenCompletions(GenCompletions),
-}
-
-#[derive(Subcommand)]
-enum GenCompletions {
-    Bash,
-    Zsh,
-    Fish,
-}
-
-#[derive(Subcommand)]
-enum ReleaseSubcommand {
-    Patch,
-    Minor,
-    Major,
-    Custom {
-        #[clap(value_parser)]
-        custom: String,
-    },
-}
+use crate::cli::Command;
+use crate::cli::VersionSpec;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::try_init()?;
 
-    let _args = Args::parse();
-    println!("Hello, world!");
+    let args = cli::get_args();
+    match args.command {
+        Command::New { .. } => {
+        }
+
+        Command::Generate(VersionSpec::Patch) => {
+        }
+
+        Command::Generate(VersionSpec::Minor) => {
+        }
+
+        Command::Generate(VersionSpec::Major) => {
+        }
+
+        Command::Generate(VersionSpec::Custom { custom: _ }) => {
+        }
+
+        Command::Release => {
+        }
+    }
 
     Ok(())
 }
