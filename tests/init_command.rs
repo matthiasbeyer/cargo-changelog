55c92541cb782fde7d63100c0cab36a992532a40
Matthias Beyer <mail@beyermatthias.de>
2022-08-27 19:12:19 +0200

    Add test for init command
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

---
+++ tests/init_command.rs
@@ -0,0 +1,29 @@
+use assert_cmd::Command;
+
+#[test]
+fn init_command_creates_config_file() {
+    let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
+
+    if !std::process::Command::new("git")
+        .args(&["init"])
+        .current_dir(&temp_dir)
+        .status()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to git-init");
+    }
+
+    Command::cargo_bin("cargo-changelog")
+        .unwrap()
+        .args(&["init"])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+
+    let config_file_path = temp_dir.path().join(".changelog.toml");
+    if !config_file_path.exists() {
+        panic!("Config file does not exist after `cargo-changelog init`");
+    }
+}
+
