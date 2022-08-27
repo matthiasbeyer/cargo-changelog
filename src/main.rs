10c33d5a5ae96fa98c07bfd1d965ffb2386ac51e
Matthias Beyer <mail@beyermatthias.de>
2022-08-27 18:49:31 +0200

    Add init command
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use miette::IntoDiagnostic;
 
 mod cli;
@@ -11,6 +13,8 @@ use crate::error::Error;
 
 fn main() -> miette::Result<()> {
     env_logger::try_init().into_diagnostic()?;
+
+    let args = cli::get_args();
 
     let cwd = std::env::current_dir()
         .map_err(Error::from)
@@ -26,6 +30,10 @@ fn main() -> miette::Result<()> {
         .into_diagnostic()?
         .to_path_buf();
 
+    if let Command::Init = args.command {
+        return init(repo_workdir_path);
+    }
+
     let config = crate::config::load(&repo_workdir_path)?;
 
     if !config.fragment_dir().exists() {
@@ -40,8 +48,9 @@ fn main() -> miette::Result<()> {
             .into_diagnostic()?;
     }
 
-    let args = cli::get_args();
     match args.command {
+        Command::Init => unreachable!(), // reached above
+
         Command::New { .. } => {}
 
         Command::Generate(VersionSpec::Patch) => {}
@@ -56,4 +65,32 @@ fn main() -> miette::Result<()> {
     }
 
     Ok(())
+}
+
+fn init(repo_workdir_path: PathBuf) -> miette::Result<()> {
+    use std::io::Write;
+
+    std::fs::create_dir_all(repo_workdir_path.join(crate::config::fragment_dir_default()))
+        .map_err(Error::from)
+        .into_diagnostic()?;
+
+    let default_config = crate::config::Configuration::default();
+    let default_config = toml::to_string(&default_config).unwrap(); // cannot happen
+
+    let mut config_file = std::fs::OpenOptions::new()
+        .create(true)
+        .append(false)
+        .write(true)
+        .open(repo_workdir_path.join(crate::config::CONFIG_FILE_NAME))
+        .map_err(Error::from)
+        .into_diagnostic()?;
+
+    write!(&mut config_file, "{}", default_config)
+        .map_err(Error::from)
+        .into_diagnostic()?;
+
+    config_file
+        .sync_all()
+        .map_err(Error::from)
+        .into_diagnostic()
 }
