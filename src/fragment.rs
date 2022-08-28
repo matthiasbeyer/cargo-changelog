62d11f394c390721ff8962841c01a3d288cea822
Matthias Beyer <mail@beyermatthias.de>
2022-08-28 14:09:19 +0200

    Add interface for reading/writing Fragment
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/fragment.rs
+++ src/fragment.rs
@@ -1,21 +1,33 @@
 use std::collections::HashMap;
+use std::io::Read;
+use std::io::Write;
 
-#[derive(Debug, serde::Serialize)]
+#[derive(Debug, getset::Getters)]
 pub struct Fragment {
-    data: HashMap<String, FragmentData>,
+    #[getset(get = "pub")]
+    header: HashMap<String, FragmentData>,
+    #[getset(get = "pub")]
     text: String,
 }
 
 impl Fragment {
     pub fn empty() -> Self {
         Fragment {
-            data: HashMap::new(),
+            header: HashMap::new(),
             text: String::new(),
         }
     }
 
     pub fn set_text(&mut self, text: String) {
         self.text = text;
+    }
+
+    pub fn from_reader<R: Read>(reader: &mut R) -> miette::Result<Self> {
+        unimplemented!()
+    }
+
+    pub fn write_to<W: Write>(&self, writer: &mut W) -> miette::Result<()> {
+        unimplemented!()
     }
 }
 
