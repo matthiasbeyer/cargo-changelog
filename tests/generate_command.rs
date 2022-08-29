9dad5bd1f5b9ca78be6dacd3eddb341fdb550d32
Matthias Beyer <mail@beyermatthias.de>
2022-08-29 08:48:33 +0200

    Add test that "generate" command moves
    This patch adds a test that the "generate" command moves the file(s)
    from the unreleased directory to a version directory.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/generate_command.rs
+++ tests/generate_command.rs
@@ -1,3 +1,5 @@
+use std::path::{Path, PathBuf};
+
 use assert_cmd::Command;
 
 mod common;
@@ -43,5 +45,96 @@ fn generate_command_creates_new_directory() {
     let release_dir = temp_dir.path().join(".changelogs").join("0.1.0");
     if !release_dir.exists() {
         panic!("Release dir '0.1.0' does not exist");
+    }
+}
+
+#[test]
+fn generate_command_moves_from_unreleased_dir() {
+    let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
+    self::common::init_git(temp_dir.path());
+
+    // create a new cargo project in the temp dir
+    Command::new("cargo")
+        .args(&[
+            "init",
+            "--bin",
+            "--name",
+            "cargo-changelog-testpkg-generatecommand",
+        ])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+
+    // initialize changelog
+    Command::cargo_bin("cargo-changelog")
+        .unwrap()
+        .args(&["init"])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+
+    Command::cargo_bin("cargo-changelog")
+        .unwrap()
+        .args(&[
+            "new",
+            "--interactive=false",
+            "--edit=false",
+            "--format=yaml",
+        ])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+
+    let unreleased_dir = temp_dir.path().join(".changelogs").join("unreleased");
+    let files_in_dir = |path: &Path| -> Vec<_> {
+        std::fs::read_dir(path)
+            .expect(&format!("Should exist: {}", path.display()))
+            .into_iter()
+            .collect::<Vec<_>>()
+    };
+
+    {
+        let files = files_in_dir(&unreleased_dir);
+        assert_eq!(
+            files.len(),
+            1,
+            "Expected 1 entries in unreleased directory, found {}: {:?}",
+            files.len(),
+            files
+        );
+    }
+
+    let released_dir = temp_dir.path().join(".changelogs").join("0.1.0");
+    if released_dir.exists() {
+        panic!("Release directory should not exist yet");
+    }
+
+    // call `cargo-changelog generate`
+    Command::cargo_bin("cargo-changelog")
+        .unwrap()
+        .args(&["generate", "minor"])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+
+    {
+        let files = files_in_dir(&unreleased_dir);
+        assert_eq!(
+            files.len(),
+            0,
+            "Expected 0 entries in unreleased directory, found {}: {:?}",
+            files.len(),
+            files
+        );
+    }
+    {
+        let files = files_in_dir(&released_dir);
+        assert_eq!(
+            files.len(),
+            1,
+            "Expected 1 entries in released directory, found {}: {:?}",
+            files.len(),
+            files
+        );
     }
 }
