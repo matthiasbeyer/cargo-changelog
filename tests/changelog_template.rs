3bf6a9f5f50f51dde555a8e2985ac2e274b4c15c
Matthias Beyer <mail@beyermatthias.de>
2022-08-29 12:00:44 +0200

    Add test that versions are sorted appropriately
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/changelog_template.rs
+++ tests/changelog_template.rs
@@ -98,3 +98,77 @@ fn default_template_renders_with_one_entry_with_header() {
         template
     );
 }
+
+#[test]
+fn default_template_renders_versions_sorted() {
+    let mut hb = Handlebars::new();
+    let mut data: HashMap<String, Vec<_>> = HashMap::new();
+    data.insert(
+        "versions".to_string(),
+        vec![
+            VersionMock {
+                version: "0.1.0".to_string(),
+                entries: vec![FragmentMock {
+                    text: "test for 0.1.0".to_string(),
+                    header: {
+                        let mut hdr = HashMap::new();
+                        hdr.insert("issue".to_string(), 123);
+                        hdr
+                    },
+                }],
+            },
+            VersionMock {
+                version: "0.2.0".to_string(),
+                entries: vec![FragmentMock {
+                    text: "test for 0.2.0".to_string(),
+                    header: {
+                        let mut hdr = HashMap::new();
+                        hdr.insert("issue".to_string(), 234);
+                        hdr
+                    },
+                }],
+            },
+        ],
+    );
+    hb.register_template_string("t", TEMPLATE).unwrap();
+    let template = hb.render("t", &data);
+    assert!(template.is_ok(), "Not ok: {:?}", template.unwrap_err());
+    let template = template.unwrap();
+
+    assert!(
+        predicates::str::contains("## v0.1.0").eval(&template),
+        "Does not contain '## v0.1.0': {}",
+        template
+    );
+    assert!(
+        predicates::str::contains("## v0.2.0").eval(&template),
+        "Does not contain '## v0.2.0': {}",
+        template
+    );
+
+    let line_number_of_010 = {
+        template
+            .lines()
+            .enumerate()
+            .filter(|(_n, line)| *line == "## v0.1.0")
+            .next()
+            .map(|(n, _)| n)
+            .unwrap()
+    };
+
+    let line_number_of_020 = {
+        template
+            .lines()
+            .enumerate()
+            .filter(|(_n, line)| *line == "## v0.2.0")
+            .next()
+            .map(|(n, _)| n)
+            .unwrap()
+    };
+
+    assert!(
+        line_number_of_020 < line_number_of_010,
+        "line with v0.1.0 should come _after_ line with v0.2.0: {}",
+        template
+    );
+}
