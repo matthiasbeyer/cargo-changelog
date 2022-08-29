148fc5c9984831fe09bb30a631e36012ea0d99cf
Matthias Beyer <mail@beyermatthias.de>
2022-08-29 08:38:33 +0200

    Add "generate" command implementation
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -5,6 +5,9 @@ use crate::config::Configuration;
 mod new_command;
 pub use self::new_command::NewCommand;
 
+mod generate_command;
+pub use self::generate_command::GenerateCommand;
+
 pub trait Command {
     fn execute(self, workdir: &Path, config: &Configuration) -> miette::Result<()>;
 }
