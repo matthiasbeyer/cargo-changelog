a1264abd5c5e02fc969820fbd73fdad2f1cef5d3
Matthias Beyer <mail@beyermatthias.de>
2022-09-01 14:31:29 +0200

    Add helper for initializing cargo project
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/common.rs
+++ tests/common.rs
@@ -9,3 +9,15 @@ pub fn init_git(temp_dir: &std::path::Path) {
         panic!("Failed to git-init");
     }
 }
+
+pub fn init_cargo(temp_dir: &std::path::Path) {
+    if !std::process::Command::new("cargo")
+        .args(&["init", "--name", "test-crate"])
+        .current_dir(temp_dir)
+        .status()
+        .unwrap()
+        .success()
+    {
+        panic!("Failed to git-init");
+    }
+}
