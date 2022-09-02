1b808256d6b31040e17a985616982f5323b081f7
Matthias Beyer <mail@beyermatthias.de>
2022-09-02 09:42:18 +0200

    Let the Handlebars creator fn return specific error
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/template/mod.rs
+++ src/template/mod.rs
@@ -1,18 +1,14 @@
 use handlebars::Handlebars;
-use miette::IntoDiagnostic;
 
 use crate::error::Error;
 
 mod reverse_helper;
 mod sort_versions_helper;
 
-pub fn new_handlebars(template_source: &str) -> miette::Result<Handlebars> {
+pub fn new_handlebars(template_source: &str) -> Result<Handlebars, Error> {
     let mut handlebars = Handlebars::new();
     handlebars.register_escape_fn(handlebars::no_escape);
-    handlebars
-        .register_template_string(crate::consts::INTERNAL_TEMPLATE_NAME, template_source)
-        .map_err(Error::from)
-        .into_diagnostic()?;
+    handlebars.register_template_string(crate::consts::INTERNAL_TEMPLATE_NAME, template_source)?;
     handlebars.register_helper(
         "sort_versions",
         Box::new(self::sort_versions_helper::sort_versions),
