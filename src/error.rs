e0b5b0658af43600f54ef298b6ff68400dd85f3f
Matthias Beyer <mail@beyermatthias.de>
2022-09-02 09:19:40 +0200

    Outsource fragment-specific error kinds
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/error.rs
+++ src/error.rs
@@ -14,12 +14,6 @@ pub enum Error {
     #[error("TOML deserialization error")]
     Toml(#[from] toml::de::Error),
 
-    #[error("TOML serialization error")]
-    TomlSer(#[from] toml::ser::Error),
-
-    #[error("YAML error")]
-    Yaml(#[from] serde_yaml::Error),
-
     #[error("Time formatting error")]
     TimeFormat(#[from] time::error::Format),
 
@@ -40,4 +34,45 @@ pub enum Error {
 
     #[error("Configuration file does not exist: {0}")]
     ConfigDoesNotExist(PathBuf),
+}
+
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+pub enum FragmentError {
+    #[error("IO")]
+    Io(#[from] std::io::Error),
+
+    #[error("Expected header seperator: '---' or '+++', found: '{0}'")]
+    ExpectedSeperator(String),
+
+    #[error("Header seperator '---' or '+++' missing")]
+    HeaderSeperatorMissing,
+
+    #[error("TOML serialization error")]
+    TomlSer(#[from] toml::ser::Error),
+
+    #[error("TOML deserialization error")]
+    TomlDe(#[from] toml::de::Error),
+
+    #[error("YAML error")]
+    Yaml(#[from] serde_yaml::Error),
+
+    #[error("Type Error: Expected {exp}, got {recv}")]
+    DataType { exp: String, recv: String },
+}
+
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+#[error("UTF8 Error with path: {}", .0.display())]
+pub struct PathUtf8Error(pub PathBuf);
+
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+#[error("Semver handling error")]
+pub struct SemverError(#[from] semver::Error);
+
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+pub enum VersionError {
+    #[error("Path UTF8 Error")]
+    Utf8(#[from] PathUtf8Error),
+
+    #[error("Error handling semver")]
+    Semver(#[from] SemverError),
 }
