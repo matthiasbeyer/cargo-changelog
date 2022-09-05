bc307e03378e95a592ad5da80051ebf9564b52c3
Matthias Beyer <mail@beyermatthias.de>
2022-09-05 11:15:46 +0200

    Implement interactive metadata providing
    Does not have an implementation for lists and maps yet.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/command/new_command.rs
+++ src/command/new_command.rs
@@ -3,15 +3,22 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
+use dialoguer::Confirm;
+use dialoguer::Input;
+
 use crate::cli::TextProvider;
 use crate::config::Configuration;
 use crate::error::Error;
 use crate::error::FragmentError;
+use crate::error::InteractiveError;
 use crate::format::Format;
 use crate::fragment::FragmentData;
+use crate::fragment::FragmentDataDesc;
+use crate::fragment::FragmentDataType;
 
 #[derive(Debug, typed_builder::TypedBuilder)]
 pub struct NewCommand {
+    interactive: bool,
     edit: bool,
     format: Format,
     text: Option<TextProvider>,
@@ -52,7 +59,9 @@ impl crate::command::Command for NewCommand {
             .filter_map(|(key, data_desc)| {
                 if let Some(default) = data_desc.default_value() {
                     if data_desc.fragment_type().matches(&default) {
-                        Some(Ok((key.clone(), default.clone())))
+                        interactive_edit(key, default, data_desc)
+                            .map_err(FragmentError::from)
+                            .transpose()
                     } else {
                         Some(Err(FragmentError::DataType {
                             exp: data_desc.fragment_type().type_name().to_string(),
@@ -60,7 +69,9 @@ impl crate::command::Command for NewCommand {
                         }))
                     }
                 } else {
-                    None
+                    interactive_provide(key, data_desc)
+                        .map_err(FragmentError::from)
+                        .transpose()
                 }
             })
             .collect::<Result<HashMap<String, FragmentData>, _>>()
@@ -115,4 +126,100 @@ fn get_editor_command() -> Result<Command, Error> {
     };
 
     Ok(Command::new(editor))
+}
+
+/// Ask interactively whether these values are okay or should be changed
+fn interactive_edit(
+    key: &str,
+    value: &FragmentData,
+    value_desc: &FragmentDataDesc,
+) -> Result<Option<(String, FragmentData)>, InteractiveError> {
+    let prompt = format!("Edit '{key}' = '{data}' ({type})?",
+        key = key,
+        data = value.display(),
+        type = value.type_name());
+
+    let confirmed = dialoguer::Confirm::new()
+        .default(true)
+        .show_default(true)
+        .with_prompt(prompt)
+        .interact_opt()
+        .map_err(InteractiveError::from)?;
+
+    match confirmed {
+        None => Err(InteractiveError::Interrupted),
+        Some(true) => Ok(Some((key.to_string(), value.clone()))),
+        Some(false) => interactive_provide(key, value_desc),
+    }
+}
+
+/// Let the user provide a value matching the description interactively
+fn interactive_provide(
+    key: &str,
+    desc: &FragmentDataDesc,
+) -> Result<Option<(String, FragmentData)>, InteractiveError> {
+    match desc.fragment_type() {
+        FragmentDataType::Bool => {
+            let mut dialoguer = Confirm::new();
+            if let Some(data) = desc.default_value() {
+                if let FragmentData::Bool(b) = data {
+                    dialoguer.default(*b);
+                } else {
+                    return Err(InteractiveError::TypeError(
+                        desc.fragment_type().clone(),
+                        data.clone(),
+                    ));
+                }
+            }
+
+            let value = if desc.required() {
+                dialoguer
+                    .interact_opt()
+                    .map_err(InteractiveError::from)?
+                    .ok_or_else(|| InteractiveError::RequiredValueNotProvided(key.to_string()))?
+            } else {
+                dialoguer.interact().map_err(InteractiveError::from)?
+            };
+
+            Ok(Some((key.to_string(), FragmentData::Bool(value))))
+        }
+        FragmentDataType::Int => {
+            let mut dialoguer = Input::<u64>::new();
+            dialoguer.with_prompt("Enter a number");
+
+            if let Some(data) = desc.default_value() {
+                if let FragmentData::Int(i) = data {
+                    dialoguer.default(*i);
+                } else {
+                    return Err(InteractiveError::TypeError(
+                        desc.fragment_type().clone(),
+                        data.clone(),
+                    ));
+                }
+            }
+
+            let value = dialoguer.interact_text().map_err(InteractiveError::from)?;
+            Ok(Some((key.to_string(), FragmentData::Int(value))))
+        }
+        FragmentDataType::Str => {
+            let mut dialoguer = Input::<String>::new();
+            dialoguer.with_prompt("Enter a text");
+
+            if let Some(data) = desc.default_value() {
+                if let FragmentData::Str(s) = data {
+                    dialoguer.default(s.to_string());
+                } else {
+                    return Err(InteractiveError::TypeError(
+                        desc.fragment_type().clone(),
+                        data.clone(),
+                    ));
+                }
+            }
+
+            let value = dialoguer.interact_text().map_err(InteractiveError::from)?;
+            Ok(Some((key.to_string(), FragmentData::Str(value))))
+        }
+        FragmentDataType::List(_) => todo!(),
+        FragmentDataType::Map(_) => todo!(),
+    }
 }
