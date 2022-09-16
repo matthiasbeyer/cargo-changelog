0adefc3bfa8c4baaf377e3b94ba28e2f36c52486
Matthias Beyer <mail@beyermatthias.de>
2022-09-16 09:37:56 +0200

    Replace manual cargo-changelog-init with helper fn
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/init_command.rs
+++ tests/init_command.rs
@@ -1,18 +1,10 @@
-use assert_cmd::Command;
-
 mod common;
 
 #[test]
 fn init_command_creates_config_file() {
     let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
     self::common::init_git(temp_dir.path());
-
-    Command::cargo_bin("cargo-changelog")
-        .unwrap()
-        .args(&["init"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success();
+    self::common::init_cargo_changelog(temp_dir.path());
 
     let config_file_path = temp_dir.path().join(".changelog.toml");
     if !config_file_path.exists() {
@@ -24,13 +16,7 @@ fn init_command_creates_config_file() {
 fn init_command_creates_fragment_dir() {
     let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
     self::common::init_git(temp_dir.path());
-
-    Command::cargo_bin("cargo-changelog")
-        .unwrap()
-        .args(&["init"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success();
+    self::common::init_cargo_changelog(temp_dir.path());
 
     let config_file_path = temp_dir.path().join(".changelogs");
     if !config_file_path.exists() {
@@ -42,13 +28,7 @@ fn init_command_creates_fragment_dir() {
 fn init_command_creates_fragment_dir_unreleased() {
     let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
     self::common::init_git(temp_dir.path());
-
-    Command::cargo_bin("cargo-changelog")
-        .unwrap()
-        .args(&["init"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success();
+    self::common::init_cargo_changelog(temp_dir.path());
 
     let config_file_path = temp_dir.path().join(".changelogs").join("unreleased");
     if !config_file_path.exists() {
@@ -60,13 +40,7 @@ fn init_command_creates_fragment_dir_unreleased() {
 fn init_command_creates_default_template() {
     let temp_dir = tempdir::TempDir::new("cargo-changelog").unwrap();
     self::common::init_git(temp_dir.path());
-
-    Command::cargo_bin("cargo-changelog")
-        .unwrap()
-        .args(&["init"])
-        .current_dir(&temp_dir)
-        .assert()
-        .success();
+    self::common::init_cargo_changelog(temp_dir.path());
 
     let template_path = temp_dir.path().join(".changelogs").join("template.md");
     if !template_path.exists() {
