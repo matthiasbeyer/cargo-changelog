306ef96c3a8d5874a0e7351518b92a2da29818e4
Matthias Beyer <mail@beyermatthias.de>
2022-09-16 08:06:19 +0200

    Add helper: init_cargo_changelog()
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- tests/common.rs
+++ tests/common.rs
@@ -21,3 +21,12 @@ pub fn init_cargo(temp_dir: &std::path::Path, name: &str) {
         panic!("Failed to cargo-init");
     }
 }
+
+pub fn init_cargo_changelog(temp_dir: &std::path::Path) {
+    assert_cmd::Command::cargo_bin("cargo-changelog")
+        .unwrap()
+        .args(&["init"])
+        .current_dir(&temp_dir)
+        .assert()
+        .success();
+}
