0d92041a4a1d9749d115d7d6c9c7b59325fbcbf5
Matthias Beyer <mail@beyermatthias.de>
2022-09-16 11:53:42 +0200

    Fix clippy: Remove unneeded clone
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/template/sort_versions_helper.rs
+++ src/template/sort_versions_helper.rs
@@ -2,7 +2,7 @@ use crate::command::VersionData;
 use handlebars::handlebars_helper;
 
 handlebars_helper!(sort_versions: |args: Vec<VersionData>| {
-    let mut args = args.clone();
+    let mut args = args;
     args.sort_by(|a, b| a.version().cmp(b.version()));
     serde_json::to_value(args).unwrap() // handlebars deserializes this for us, so we can serialize
                                         // it back without issue
