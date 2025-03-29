19c6bbfd9761cd5a716d8b8e38273d0a10475027
Matthias Beyer <mail@beyermatthias.de>
2025-03-29 11:15:21 +0100

    Elide unnecessary lifetime
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/fragment.rs
+++ src/fragment.rs
@@ -123,7 +123,7 @@ impl FragmentData {
 
 pub struct FragmentDataDisplay<'a>(&'a FragmentData);
 
-impl<'a> std::fmt::Display for FragmentDataDisplay<'a> {
+impl std::fmt::Display for FragmentDataDisplay<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.0 {
             FragmentData::Bool(b) => write!(f, "{b}"),
