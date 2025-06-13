f61d8b412d6c6ee9c6f5da9fd77d0e10f4ba0c0a
Matthias Beyer <mail@beyermatthias.de>
2025-06-13 11:49:48 +0200

    Update flake input: nixpkgs: 24.11 -> 25.05
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "The cargo-changelog binary";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
       url = "github:ipetkov/crane";
